fa712169a78bdf0ce16b6084adb387edc57eb59e
Konstantin Runov <runebone1@gmail.com>
2023-08-10 16:20:21 +0300

    add ui (using imgui)

---
+++ code/src/ui.h
@@ -0,0 +1,32 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+#include <glm/glm.hpp>
+
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui.h"
+
+#include "GLFW/glfw3.h"
+
+class UI {
+public:
+    UI(GLFWwindow *window);
+
+    UI(UI &&) = delete;
+    UI(const UI &) = delete;
+    UI &operator=(UI &&) = delete;
+    UI &operator=(const UI &) = delete;
+
+    ~UI() = default;
+
+    void render(bool &show_demo_window, glm::vec4 clear_color, ImGuiIO &io);
+
+private:
+    GLFWwindow *m_window;
+    const char *font_filename = "../../assets/fonts/mononoki-Regular.ttf";
+};
+
+void init_imgui(GLFWwindow *window);
+
+#endif // __MENU_H__
