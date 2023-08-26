488f058db8725a654568f9de72556bc933cc423a
Konstantin Runov <runebone1@gmail.com>
2023-08-26 18:11:37 +0300

    refactor using RenderData;add namespace;more stuff

--- code/src/render.cpp
+++ code/src/render.cpp
@@ -7,20 +7,11 @@ static f32 r() {
     return (f32)rand() / (f32)RAND_MAX;
 }
 
-/* void render(const Mesh &mesh, const Camera &camera, Shader &shader) { */
-void render(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::mat4 &m) {
-    GL_CALL(glBindVertexArray(mesh.VAO));
-    GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.EBO));
-
-    auto rc = glm::vec3(r(), r(), r());
-    /* auto rc = glm::vec3(1.0, 0.5, 0.3); */
-    /* shader.setVec3("vColor", rc); */
-
-    bool render_by_triangles = true;
-
-    for (u32 i = 0; render_by_triangles && i < mesh.indices.size(); i += 3) {
-        auto rc = glm::vec3(r(), r(), r());
-        auto face = mesh.get_face(i / 3);
+namespace Lambert
+{
+    static glm::vec3 get_color(u32 vertex_index, const Mesh &mesh, const Camera &camera, const glm::mat4 &m)
+    {
+        auto face = mesh.get_face(vertex_index / 3);
 
         /* auto v1 = face.x.Position; */
         /* auto v2 = face.y.Position; */
@@ -34,45 +25,50 @@ void render(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::m
         auto v2 = glm::vec3(v2i);
         auto v3 = glm::vec3(v3i);
 
-        /* dbg::print(v1); */
-        /* dbg::print(v2); */
-        /* dbg::print(v3); */
-
         auto cr = glm::cross(v3 - v1, v2 - v1);
         auto n = glm::normalize(cr);
         /* auto l = glm::vec3(0, 0, 1); */
         auto l = -camera.pos;
         auto nl = glm::normalize(l);
-        auto e = glm::vec3(1, 0.6, 0.7);
+        /* auto e = glm::vec3(1, 0.6, 0.7); */
+        /* auto e = rc; */
+        auto e = glm::vec3(1, 1, 1);
         auto s = glm::dot(n, nl);
         auto c = s * e;
 
-        if (0)
-        {
-            dbg::print(face);
-            printf("Cross: ");
-            dbg::print(cr);
-            printf("N: ");
-            dbg::print(n);
-            printf("l: ");
-            dbg::print(l);
-            printf("nl: ");
-            dbg::print(nl);
-            printf("s: %f\n", s);
-            printf("e: ");
-            dbg::print(e);
-            printf("c: ");
-            dbg::print(c);
-        }
+        return c;
+    }
+}
 
-        /* shader.setVec3("vColor", rc); */
+static void render_by_trianges(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::mat4 &m)
+{
+    for (u32 i = 0; i < mesh.indices.size(); i += 3) {
+        /* auto rc = glm::vec3(r(), r(), r()); */
+
+        auto c = Lambert::get_color(i, mesh, camera, m);
+
         shader.setVec3("vColor", c);
 
         GL_CALL(glDrawElements(*mesh.drawing_mode, 3, GL_UNSIGNED_INT, (void*)(sizeof(GLuint)*i)));
     }
+}
 
-    if (!render_by_triangles)
+/* void render(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::mat4 &m) { */
+void render(const RenderData &rd) {
+    auto &mesh = rd.mesh;
+    auto &camera = rd.camera;
+    auto &shader = rd.shader;
+    auto &m = rd.matrix;
+
+    GL_CALL(glBindVertexArray(mesh.VAO));
+    GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.EBO));
+
+    if (rd.render_by_triangles) {
+    /* if (1) { */
+        render_by_trianges(mesh, camera, shader, m);
+    } else {
         GL_CALL(glDrawElements(*mesh.drawing_mode, static_cast<u32>(mesh.indices.size()), GL_UNSIGNED_INT, 0));
+    }
 
     GL_CALL(glBindVertexArray(0));
 }
