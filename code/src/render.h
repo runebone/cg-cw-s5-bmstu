c438de220c2f62ea62647901c31e8fb952945dec
Konstantin Runov <runebone1@gmail.com>
2023-08-26 18:11:12 +0300

    add RenderData struct

--- code/src/render.h
+++ code/src/render.h
@@ -6,7 +6,23 @@
 #include "mesh.h"
 #include "camera.h"
 
-/* void render(const Mesh &mesh, const Camera &camera, Shader &shader); //, const glm::mat4 &mvp); */
-void render(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::mat4 &m);
+struct RenderData
+{
+    RenderData(const Mesh &_mesh, const Camera &_camera, Shader &_shader, const bool &_render_by_triangles)
+        : mesh(_mesh), camera(_camera), shader(_shader), render_by_triangles(_render_by_triangles)
+    {
+    }
+    const Mesh &mesh;
+    const Camera &camera;
+    Shader &shader;
+
+    glm::mat4 matrix;
+
+    const bool &render_by_triangles;
+};
+
+/* void render(const Mesh &mesh, const Camera &camera, Shader &shader); */
+/* void render(const Mesh &mesh, const Camera &camera, Shader &shader, const glm::mat4 &m); */
+void render(const RenderData &rd);
 
 #endif // __RENDER_H__
