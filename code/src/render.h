55fcabbe5cd77e9cfc5762c74b7fa8fb8c25ce41
Konstantin Runov <runebone1@gmail.com>
2023-08-27 18:18:55 +0300

    remove const from RenderData

--- code/src/render.h
+++ code/src/render.h
@@ -8,12 +8,12 @@
 
 struct RenderData
 {
-    RenderData(const Mesh &_mesh, const Camera &_camera, Shader &_shader, const bool &_render_by_triangles)
+    RenderData(/*const*/ Mesh &_mesh, /*const*/ Camera &_camera, Shader &_shader, const bool &_render_by_triangles)
         : mesh(_mesh), camera(_camera), shader(_shader), render_by_triangles(_render_by_triangles)
     {
     }
-    const Mesh &mesh;
-    const Camera &camera;
+    /*const*/ Mesh &mesh;
+    /*const*/ Camera &camera;
     Shader &shader;
 
     glm::mat4 matrix;
