8332ceba79b083734b1ab4a716f82ede3fde463b
Konstantin Runov <runebone1@gmail.com>
2023-11-16 16:37:53 +0300

    remove lsp warnings

--- code/src/debug.h
+++ code/src/debug.h
@@ -8,20 +8,20 @@
 
 namespace dbg {
 
-void print(const glm::vec3 &v) {
+inline void print(const glm::vec3 &v) {
     printf("%f %f %f\n", v.x, v.y, v.z);
 }
 
-void print(const glm::vec4 &v) {
+inline void print(const glm::vec4 &v) {
     printf("%f %f %f %f\n", v.x, v.y, v.z, v.w);
 }
 
-void print(const Vertex &v) {
+inline void print(const Vertex &v) {
     printf("Vertex pos: ");
     print(v.Position);
 }
 
-void print(const face_t &f) {
+inline void print(const face_t &f) {
     printf("Face:\n");
     print(f.x);
     print(f.y);
