ba6465c2ab36907aa9f9d18cbbbf8a4c8d5dd9ae
Konstantin Runov <runebone1@gmail.com>
2023-12-13 23:13:41 +0300

    update main.py

--- code/main.py
+++ code/main.py
@@ -3,40 +3,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def process_and_plot_data(file_paths):
+    # perm = [2, 4, 1, 5, 3, 0]
     colors = ['tab:red', 'tab:orange', 'tab:green', 'tab:blue', 'tab:purple', 'tab:pink']
     markers = ['+', '^', 's', 'o', 'x', 'D']
 
     _, axes = plt.subplots(2, 2, figsize=(14, 10))
 
     # for i, file in enumerate(file_paths):
-    for i, fi in enumerate([2, 4, 1, 5, 3, 0]):
+    for fi, i in enumerate([2, 4, 1, 5, 3, 0]):
+        # if fi in [4, 5]: continue
+        if fi in [0, 1, 2]: continue
         # data = pd.read_csv(file)
         data = pd.read_csv(file_paths[fi])
         grouped_data = data.groupby('n_objects')[['n_triangles', 'n_collisions', 'n_draw_calls', 'time_ns']].mean().reset_index()
 
-        axes[0, 0].plot(grouped_data['n_objects'], grouped_data['n_triangles'], marker=markers[i], color=colors[i], label=f'File {i+1}')
-        axes[0, 1].plot(grouped_data['n_objects'], grouped_data['n_collisions'], marker=markers[i], color=colors[i])
-        axes[1, 0].plot(grouped_data['n_objects'], grouped_data['n_draw_calls'], marker=markers[i], color=colors[i])
-        axes[1, 1].plot(grouped_data['n_objects'], grouped_data['time_ns'], marker=markers[i], color=colors[i])
+        # grouped_data['time_ns'] = list(map(lambda x: x / 10, grouped_data['time_ns']))
 
-    axes[0, 0].set_title('Average Number of Triangles vs Number of Objects')
-    axes[0, 0].set_xlabel('Number of Objects')
-    axes[0, 0].set_ylabel('Average Number of Triangles')
+        axes[0, 0].plot(grouped_data['n_objects'], grouped_data['n_triangles'], marker=markers[i], color=colors[i], label=f'Тест {fi+1}')
+        axes[0, 1].plot(grouped_data['n_objects'], grouped_data['n_collisions'], marker=markers[i], color=colors[i], label=f'Тест {fi+1}')
+        axes[1, 0].plot(grouped_data['n_objects'], grouped_data['n_draw_calls'], marker=markers[i], color=colors[i], label=f'Тест {fi+1}')
+        axes[1, 1].plot(grouped_data['n_objects'], grouped_data['time_ns'], marker=markers[i], color=colors[i], label=f'Тест {fi+1}')
+
+    # axes[0, 0].set_title('Average Number of Triangles vs Number of Objects')
+    axes[0, 0].set_xlabel('Количество объектов')
+    axes[0, 0].set_ylabel('Среднее количество треугольников')
     axes[0, 0].set_yscale('log')
 
-    axes[0, 1].set_title('Average Number of Collisions vs Number of Objects')
-    axes[0, 1].set_xlabel('Number of Objects')
-    axes[0, 1].set_ylabel('Average Number of Collisions')
-    axes[0, 1].set_yscale('log')
+    # axes[0, 1].set_title('Average Number of Collisions vs Number of Objects')
+    axes[0, 1].set_xlabel('Количество объектов')
+    axes[0, 1].set_ylabel('Среднее количество коллизий')
+    # axes[0, 1].set_yscale('log')
 
-    axes[1, 0].set_title('Average Number of Draw Calls vs Number of Objects')
-    axes[1, 0].set_xlabel('Number of Objects')
-    axes[1, 0].set_ylabel('Average Number of Draw Calls')
+    # axes[1, 0].set_title('Average Number of Draw Calls vs Number of Objects')
+    axes[1, 0].set_xlabel('Количество объектов')
+    axes[1, 0].set_ylabel('Среднее количество вызовов функций отрисовки OpenGL')
     axes[1, 0].set_yscale('log')
 
-    axes[1, 1].set_title('Average Time (ns) vs Number of Objects')
-    axes[1, 1].set_xlabel('Number of Objects')
-    axes[1, 1].set_ylabel('Average Time (ns)')
+    # axes[1, 1].set_title('Average Time (ns) vs Number of Objects')
+    axes[1, 1].set_xlabel('Количество объектов')
+    axes[1, 1].set_ylabel('Среднее время генерации кадра (мс)')
     axes[1, 1].set_yscale('log')
 
     for ax in axes.flat:
